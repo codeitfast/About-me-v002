fcff03189c0de1ef338cc39aeeff32515f932f11
codeitfast <74981041+codeitfast@users.noreply.github.com>
2022-03-21 04:41:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$( document ).ready(function(){
+  setTimeout(function(){
+    $("#python").addClass("show")
+  }, 0)
+  setTimeout(function(){
+    $("#javascript").addClass("show")
+  }, 400)
+  setTimeout(function(){
+    $("#html").addClass("show")
+  }, 800)
+  setTimeout(function(){
+    $("#css").addClass("show")
+  }, 1200)
+})
